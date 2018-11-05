7289171ee8ecd1eae1efc699f177feacbe7df0ae
oscarpimentel <32822761+oscarpimentel@users.noreply.github.com>
2018-11-05 00:06:51 -0300

    Add files via upload

---
+++ fredi_rx/fredi_rx.ino
@@ -0,0 +1,26 @@
+#include <FrediLib.h>
+ 
+//Declaremos los pines CE y el CSN
+#define CE_PIN 9
+#define CSN_PIN 10
+ 
+FrediRx frediRx = FrediRx(CE_PIN, CSN_PIN);
+
+void setup()
+{
+  Serial.begin(9600); 
+  frediRx.begin();
+  delay(200);
+}
+ 
+void loop() {
+ uint8_t numero_canal;
+ //if ( radio.available(&numero_canal) )
+ if(frediRx.available()){    
+     frediRx.read();
+     frediRx.printData();
+ }else{
+     //Serial.println("No hay datos de radio disponibles");
+ }
+ delay(250);
+}
